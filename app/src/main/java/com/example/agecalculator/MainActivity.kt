943003f73203223c77d97faf46e45d9b37b7f2c2
minuszero <pcijideani@gmail.com>
2021-05-27 20:50:49 +0200

    some mods

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -5,15 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.TextFieldValue
@@ -25,34 +25,54 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             MainScreen()
         }
     }
 }
 
 @Composable
+fun AppBar(title: String){
+    TopAppBar(title = { Text(text = "AppBar", color = Color.White)}, navigationIcon = { IconButton(
+        onClick = { /*TODO*/ }) {
+
+    }})
+
+}
+
+
+
+@Composable
 fun MainScreen(){
 
-    Surface(color = Color.DarkGray,
+   Surface(color = Color.LightGray,
         modifier = Modifier.fillMaxSize()) {
 
-        Column {
+        
+
+
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .padding(64.dp), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally){
+
 
             val mytext = remember { mutableStateOf(TextFieldValue()) }
             TextField(value = mytext.value,  onValueChange = { mytext.value}, label = {Text("enter year of birth")})
 
-            Surface(color = Color.LightGray, modifier = Modifier.wrapContentSize()) {
+            //Surface(color = Color.LightGray, modifier = Modifier.wrapContentSize()) {
                 Button(
                     onClick = { /*TODO*/ },
                     shape = MaterialTheme.shapes.medium,
-                    border = BorderStroke(width = 4.dp, brush = SolidColor(Color.Red))
+                    border = BorderStroke(
+                        width = 4.dp,
+                        brush = SolidColor(Color.Red))
                 ) {
                     Text("Calculate Age")
 
                 }
-            }
+            //}
         }
-    }    
+   }
 }
 
 
