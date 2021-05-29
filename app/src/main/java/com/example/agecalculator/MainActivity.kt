1f60f220a9b0343eb5763598b7bcab5ae07eeec2
minuszero <pcijideani@gmail.com>
2021-05-29 21:19:26 +0200

    some mods

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.example.agecalculator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.agecalculator.ui.theme.AgeCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +34,6 @@ fun AppBar(){
 
 }
 
-
-
 @Composable
 fun MainScreen(){
 
@@ -56,11 +50,11 @@ fun MainScreen(){
             .padding(64.dp), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally){
 
 
-            val mytext = remember { mutableStateOf(TextFieldValue()) }
-            TextField(value = mytext.value,  onValueChange = { mytext.value}, label = {Text("enter year of birth")})
+            val inputValue = remember { mutableStateOf(TextFieldValue()) }
+            TextField(value = inputValue.value,  onValueChange = { inputValue.value = it}, placeholder = {Text("enter year of birth")})
 
                 Button(
-                    onClick = { /*TODO*/ },
+                    onClick = { Log.d("tag", "Button Clicked") },
                     colors = ButtonDefaults.textButtonColors(backgroundColor = Color.Transparent),
                     shape = RoundedCornerShape(8.dp),
                     modifier = Modifier.fillMaxWidth(),
@@ -73,6 +67,11 @@ fun MainScreen(){
             }
        }
    }
+}
+
+@Composable
+fun calculate(){
+
 }
 
 
